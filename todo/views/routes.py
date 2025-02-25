0556144e44200be6b68a9927522b65091883d962
Deven Bhasin <140460635+dbhasin4123@users.noreply.github.com>
2025-02-25 05:48:56 +0000

    Fixes

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -6,6 +6,18 @@ api = Blueprint('api', __name__, url_prefix='/api/v1')
 def health(): 
     return jsonify({"status": "ok"})
 
+@api.route('/todos', methods=['GET']) 
+def get_todos(): 
+    return jsonify([{ 
+        "id":1, 
+        "title": "Watch CSSE6400 Lecture", 
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": True,
+        "deadline_at": "2023-02-27 T00:00:00",
+        "created_at": "2023-02-20 T00:00:00", 
+        "updated_at": "2023-02-20 T00:00:00"
+         }])
+
 @api.route('/todos/<int:id>', methods=['GET']) 
 def get_todos(id): 
     return jsonify([{ 
