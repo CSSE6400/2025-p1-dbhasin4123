4556ef4a0987bb43871d498eb9ca9879c78709f7
Deven Bhasin <140460635+dbhasin4123@users.noreply.github.com>
2025-02-25 12:58:21 +0000

    All working

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -5,10 +5,21 @@ api = Blueprint('api', __name__, url_prefix='/api/v1')
 @api.route('/health') 
 def health(): 
     return jsonify({"status": "ok"})
-    
+
+@api.route('/todos', methods=['GET'])
+def get_todos(): 
+    return jsonify([{ 
+        "id":1, 
+        "title": "WatchCSSE6400Lecture", 
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
+        "completed": True, 
+        "deadline_at": "2023-02-27T00:00:00", 
+        "created_at": "2023-02-20T00:00:00", 
+        "updated_at": "2023-02-20T00:00:00" 
+        }])    
 
 @api.route('/todos/<int:id>', methods=['GET']) 
-def get_todos(id): 
+def get_todo(id): 
     return jsonify([{ 
         "id":id, 
         "title": "Watch CSSE6400 Lecture", 
@@ -20,7 +31,7 @@ def get_todos(id):
          }])
 
 @api.route('/todos', methods=['POST']) 
-def create_todos(): 
+def create_todo(): 
     return jsonify([{ 
         "id":1, 
         "title": "Watch CSSE6400 Lecture", 
@@ -29,10 +40,10 @@ def create_todos():
         "deadline_at": "2023-02-27 T00:00:00",
         "created_at": "2023-02-20 T00:00:00", 
         "updated_at": "2023-02-20 T00:00:00"
-         }])
+         }]), 201
 
 @api.route('/todos/<int:id>', methods=['PUT']) 
-def update_todos(id): 
+def update_todo(id): 
     return jsonify([{ 
         "id":id, 
         "title": "Watch CSSE6400 Lecture", 
@@ -44,7 +55,7 @@ def update_todos(id):
          }])
 
 @api.route('/todos/<int:id>', methods=['DELETE']) 
-def delete_todos(id): 
+def delete_todo(id): 
     return jsonify([{ 
         "id":id, 
         "title": "Watch CSSE6400 Lecture", 
